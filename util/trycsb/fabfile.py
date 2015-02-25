e11a626cb07ef30cdee14490b9cc217a97a3affe
Serj Sintsov <ssivikt@gmail.com>
2015-02-25 11:25:56 +0300

    lib: add clear logs tasks; fix tar

--- util/trycsb/fabfile.py
+++ util/trycsb/fabfile.py
@@ -5,5 +5,6 @@ from lib.setup_cycle import setup_env
 
 from lib.benchmark_cycle import benchmark_deploy
 from lib.benchmark_cycle import benchmark_run
+from lib.benchmark_cycle import benchmark_drop_logs
 
 import var_tasks
