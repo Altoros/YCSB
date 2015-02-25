338c4f443ed3e892bfe81aab39a60c08d4c46178
Aleksei Akhrymenka <aleksei.akhrymenka@altoros.com>
2015-02-25 22:24:18 +0300

    update couchbase scripts

--- util/trycsb/setup_scripts/couchbase/ubuntu/bin/cluster_config.sh
+++ util/trycsb/setup_scripts/couchbase/ubuntu/bin/cluster_config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export PATH=$PATH:/opt/couchbase/bin
+
 if [ -z "$1" ]; then
     echo "Username required"
     exit 1
@@ -26,13 +28,13 @@ COUCHBASE_CLUSTER_RAMSIZE="1000"
 
 COUCHBASE_BUCKET_NAME="default"
 COUCHBASE_BUCKET_TYPE="couchbase"
-COUCHBASE_BUCKET_RAMSIZE="1000"
+COUCHBASE_BUCKET_RAMSIZE="300"
 
 echo "node initialization"
 couchbase-cli node-init -c ${CURRENT_NODE}:${PORT} --node-init-data-path=${COUCHBASE_DATA_PATH} --node-init-index-path=${COUCHBASE_INDEX_PATH} -u ${USER} -p ${PASS}
 
 echo "cluster initialization"
-couchbase-cli cluster-init -c ${CURRENT_NODE}:${PORT} --cluster-username=${COUCHBASE_CLUSTER_USERNAME} --cluster-password=${COUCHBASE_CLUSTER_PASSWORD} --cluster-ramsize=${COUCHBASE_CLUSTER_RAMSIZE}
+couchbase-cli cluster-init -c ${CURRENT_NODE}:${PORT} --cluster-username=${COUCHBASE_CLUSTER_USERNAME} --cluster-password=${COUCHBASE_CLUSTER_PASSWORD} --cluster-ramsize=${COUCHBASE_CLUSTER_RAMSIZE} -u ${USER} -p ${PASS}
 
 echo "create bucket"
 couchbase-cli bucket-create -c ${CURRENT_NODE}:${PORT} --bucket=${COUCHBASE_BUCKET_NAME} --bucket-type=${COUCHBASE_BUCKET_TYPE} --bucket-ramsize=${COUCHBASE_BUCKET_RAMSIZE} -u ${USER} -p ${PASS}
@@ -40,9 +42,3 @@ couchbase-cli bucket-create -c ${CURRENT_NODE}:${PORT} --bucket=${COUCHBASE_BUCK
 echo "add nodes to cluster"
 couchbase-cli server-add -c ${CURRENT_NODE}:${PORT} --server-add=${NODE1}:${PORT} -u ${USER} -p ${PASS}
 couchbase-cli server-add -c ${CURRENT_NODE}:${PORT} --server-add=${NODE2}:${PORT} -u ${USER} -p ${PASS}
-
-echo "list servers in cluster"
-couchbase-cli server-list -c ${CURRENT_NODE}:${PORT}
-
-echo "server information"
-couchbase-cli server-info -c ${CURRENT_NODE}:${PORT} -u ${USER} -p ${PASS}
--- util/trycsb/setup_scripts/couchbase/ubuntu/bin/uninstall_couchbase.sh
+++ util/trycsb/setup_scripts/couchbase/ubuntu/bin/uninstall_couchbase.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 echo "Uninstalling Couchbase"
-
 service couchbase-server stop
-
-rm /etc/apt/sources.list.d/couchbase.list
+dpkg -r couchbase-server
 rm -R /opt/couchbase
-apt-get remove -y --purge couchbase-server
-apt-get remove -y --purge autoremove
