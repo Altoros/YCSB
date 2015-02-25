60cfad0f378e4cda8af3658e3d15c357dd323740
Aleksei Akhrymenka <aleksei.akhrymenka@altoros.com>
2015-02-25 18:04:25 +0300

    update couchbase scripts

--- util/trycsb/setup_scripts/couchbase/ubuntu/setup_couchbase.sh
+++ util/trycsb/setup_scripts/couchbase/ubuntu/setup_couchbase.sh
@@ -1,31 +1,15 @@
 #!/bin/bash
 
-#bash bin/uninstall_couchbase.sh
-#bash bin/install_couchbase.sh
-#bash bin/cluster_config.sh
+if [ -z "$1" ]; then
+    echo "Username required"
+    exit 1
+fi
 
+if [ -z "$2" ]; then
+    echo "Password required"
+    exit 1
+fi
 
-#CURRENT_NODE=192.155.206.162
-#NODE1=192.155.206.163
-#NODE2=50.23.195.162
-#PORT=8091
-#USER=altoros
-#PASS=changeme
-
-#COUCHBASE_DATA_PATH=/tmp/data
-#COUCHBASE_INDEX_PATH=/tmp/index
-
-#COUCHBASE_CLUSTER_USERNAME=user
-#COUCHBASE_CLUSTER_PASSWORD=pass
-#COUCHBASE_CLUSTER_RAMSIZE=xxx
-
-#COUCHBASE_BUCKET_NAME=default
-#COUCHBASE_BUCKET_TYPE=couchbase
-#COUCHBASE_BUCKET_RAMSIZE=xxx
-
-# couchbase limits
-# Max key length: 250 bytes
-# Max value size: 20 MB
-# Max bucket size: unlimited
-# Max Buckets per Cluster (default): 10*
-# Max View Key Size: 4096 bytes
+bash bin/uninstall_couchbase.sh
+bash bin/install_couchbase.sh
+bash bin/cluster_config.sh $1 $2
