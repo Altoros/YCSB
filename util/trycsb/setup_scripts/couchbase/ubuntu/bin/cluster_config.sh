41c9e6fcb5794feeff47cc765aac0d9283809051
Aleksei Akhrymenka <aleksei.akhrymenka@altoros.com>
2015-02-26 23:08:22 +0300

    update couchbase scripts

--- util/trycsb/setup_scripts/couchbase/ubuntu/bin/cluster_config.sh
+++ util/trycsb/setup_scripts/couchbase/ubuntu/bin/cluster_config.sh
@@ -2,33 +2,33 @@
 
 export PATH=$PATH:/opt/couchbase/bin
 
-CURRENT_NODE="192.155.206.162"
-NODE1="192.155.206.163"
-NODE2="50.23.195.162"
-PORT="8091"
-USER="couchbase"
-PASS="couchbase"
+COUCHBASE_CURRENT_NODE="192.155.206.162"
+COUCHBASE_NODE1="192.155.206.163"
+COUCHBASE_NODE2="50.23.195.162"
+COUCHBASE_PORT="8091"
+COUCHBASE_USER="couchbase"
+COUCHBASE_PASSWORD="couchbase"
 
-COUCHBASE_DATA_PATH=/disk1/data
-COUCHBASE_INDEX_PATH=/tmp/index
+COUCHBASE_DATA_PATH=/opt/couchbase/var/lib/couchbase/data
+COUCHBASE_INDEX_PATH=/disk1/couchbase/index
 
-COUCHBASE_CLUSTER_USERNAME=${USER}
-COUCHBASE_CLUSTER_PASSWORD=${PASS}
-COUCHBASE_CLUSTER_RAMSIZE="10000"
+#TODO create path with $mkdir and $chown to couchbase
+
+COUCHBASE_CLUSTER_RAMSIZE="1000"
 
 COUCHBASE_BUCKET_NAME="default"
 COUCHBASE_BUCKET_TYPE="couchbase"
-COUCHBASE_BUCKET_RAMSIZE="7000"
+COUCHBASE_BUCKET_RAMSIZE="700"
 
 echo "node initialization"
-couchbase-cli node-init -c ${CURRENT_NODE}:${PORT} --node-init-data-path=${COUCHBASE_DATA_PATH} --node-init-index-path=${COUCHBASE_INDEX_PATH} -u ${USER} -p ${PASS}
+couchbase-cli node-init -c ${COUCHBASE_CURRENT_NODE}:${COUCHBASE_PORT} --node-init-data-path=${COUCHBASE_DATA_PATH} --node-init-index-path=${COUCHBASE_INDEX_PATH} -u ${COUCHBASE_USER} -p ${COUCHBASE_PASSWORD}
 
 echo "cluster initialization"
-couchbase-cli cluster-init -c ${CURRENT_NODE}:${PORT} --cluster-username=${COUCHBASE_CLUSTER_USERNAME} --cluster-password=${COUCHBASE_CLUSTER_PASSWORD} --cluster-ramsize=${COUCHBASE_CLUSTER_RAMSIZE} -u ${USER} -p ${PASS}
+couchbase-cli cluster-init -c ${COUCHBASE_CURRENT_NODE}:${COUCHBASE_PORT} --cluster-username=${COUCHBASE_USER} --cluster-password=${COUCHBASE_PASSWORD} --cluster-ramsize=${COUCHBASE_CLUSTER_RAMSIZE} -u ${COUCHBASE_USER} -p ${COUCHBASE_PASSWORD}
 
 echo "create bucket"
-couchbase-cli bucket-create -c ${CURRENT_NODE}:${PORT} --bucket=${COUCHBASE_BUCKET_NAME} --bucket-type=${COUCHBASE_BUCKET_TYPE} --bucket-ramsize=${COUCHBASE_BUCKET_RAMSIZE} -u ${USER} -p ${PASS}
+couchbase-cli bucket-create -c ${COUCHBASE_CURRENT_NODE}:${COUCHBASE_PORT} --bucket=${COUCHBASE_BUCKET_NAME} --bucket-type=${COUCHBASE_BUCKET_TYPE} --bucket-ramsize=${COUCHBASE_BUCKET_RAMSIZE} -u ${COUCHBASE_USER} -p ${COUCHBASE_PASSWORD}
 
 echo "add nodes to cluster"
-couchbase-cli server-add -c ${CURRENT_NODE}:${PORT} --server-add=${NODE1}:${PORT} -u ${USER} -p ${PASS}
-couchbase-cli server-add -c ${CURRENT_NODE}:${PORT} --server-add=${NODE2}:${PORT} -u ${USER} -p ${PASS}
+couchbase-cli server-add -c ${COUCHBASE_CURRENT_NODE}:${COUCHBASE_PORT} --server-add=${COUCHBASE_NODE1}:${COUCHBASE_PORT} -u ${COUCHBASE_USER} -p ${COUCHBASE_PASSWORD}
+couchbase-cli server-add -c ${COUCHBASE_CURRENT_NODE}:${COUCHBASE_PORT} --server-add=${COUCHBASE_NODE2}:${COUCHBASE_PORT} -u ${COUCHBASE_USER} -p ${COUCHBASE_PASSWORD}
--- util/trycsb/setup_scripts/couchbase/ubuntu/bin/uninstall_couchbase.sh
+++ util/trycsb/setup_scripts/couchbase/ubuntu/bin/uninstall_couchbase.sh
@@ -4,3 +4,4 @@ echo "Uninstalling Couchbase"
 service couchbase-server stop
 dpkg -r couchbase-server
 rm -R /opt/couchbase
+#TODO $rm -R $COUCHBASE_DATA_PATH and $COUCHBASE_INDEX_PATH
