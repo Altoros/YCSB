55dee7703fc0aefa3e3e64c67a7f42bfcd282c8a
akaravaev <dvlp.kara@gmail.com>
2015-03-22 16:20:14 +0300

    dynamic db profiles, not workloads.

--- util/trycsb/run_multiple_clients.sh
+++ util/trycsb/run_multiple_clients.sh
@@ -4,6 +4,7 @@ script_name=`basename $0`
 clients_count=${1}
 workload_name=${2}
 db_profile=${3}
+read -a PROFILES <<< ${db_profile}
 
 if [ -z "$clients_count"  ];
 then
@@ -24,5 +25,5 @@ fi
 
 for i in `seq 1 $clients_count`;
 do
-    fab benchmark_run:workload_name=${workload_name}_${i},db_profile=${db_profile} > "${workload_name}_${i}.log" 2>&1 &
+    fab benchmark_run:workload_name=${workload_name},db_profile=${PROFILES[i - 1]} > "${workload_name}_${i}.log" 2>&1 &
 done
