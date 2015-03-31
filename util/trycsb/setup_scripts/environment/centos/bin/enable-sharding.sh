9323159b33df05ba146dfcdd21d56e893768e211
akaravaev <anton.karavaev@altoros.com>
2015-03-31 17:57:28 +0300

    run second mongos process on the client.

--- util/trycsb/setup_scripts/environment/centos/bin/enable-sharding.sh
+++ util/trycsb/setup_scripts/environment/centos/bin/enable-sharding.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 mongos --configdb 192.155.206.162:57000,192.155.206.163:57000,50.23.195.162:57000 --fork --logpath /disk1/mongodb-logs/mongos.log --noAutoSplit
+mongos --port 37017 --configdb 192.155.206.162:57000,192.155.206.163:57000,50.23.195.162:57000 --fork --logpath /disk1/mongodb-logs/mongos2.log --noAutoSplit
 
 mongo << 'EOF'
     sh.addShard("shard0/192.155.206.162:27000,50.23.195.162:27000")
