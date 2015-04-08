cbf6d5f434e6c82f3cdd0370278096124f8c67a7
Serj Sintsov <ssivikt@gmail.com>
2015-04-08 22:49:19 +0300

    couchbase: impl update method (use replace)

--- dbs/couchbase-3.0/src/main/java/com/yahoo/ycsb/couchbase/CouchbaseClient.java
+++ dbs/couchbase-3.0/src/main/java/com/yahoo/ycsb/couchbase/CouchbaseClient.java
@@ -5,6 +5,7 @@ import com.couchbase.client.java.Cluster;
 import com.couchbase.client.java.CouchbaseCluster;
 import com.couchbase.client.java.document.JsonDocument;
 import com.couchbase.client.java.document.json.JsonObject;
+import com.couchbase.client.java.error.CASMismatchException;
 import com.yahoo.ycsb.ByteIterator;
 import com.yahoo.ycsb.DBException;
 import com.yahoo.ycsb.StringByteIterator;
@@ -98,20 +99,19 @@ public class CouchbaseClient extends MemcachedCompatibleClient {
 
         while (true) {
             JsonDocument doc = defaultBucket.get(qualifiedKey);
-            long origCas = doc.cas();
+            if (doc == null)
+                return ERROR;
 
             for (Map.Entry<String, ByteIterator> field : values.entrySet())
                 doc.content().put(field.getKey(), field.getValue().toString());
 
             try {
-                JsonDocument origDoc = defaultBucket.upsert(doc, persistTo, replicateTo);
-
-                if (origCas != origDoc.cas()) {
-                    incDocUpdateRace(key);
-                    sleep(config.getConcurrentUpdateRetryTimeMillis());
-                }
-                else
-                    return OK;
+                defaultBucket.replace(doc, persistTo, replicateTo);
+                return OK;
+            }
+            catch (CASMismatchException ce) {
+                incDocUpdateRace(key);
+                sleep(config.getConcurrentUpdateRetryTimeMillis());
             }
             catch (Exception e) {
                 e.printStackTrace();
