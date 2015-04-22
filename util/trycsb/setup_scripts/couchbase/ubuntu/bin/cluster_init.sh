70b0a5de430de1bcb8122e4996395d353810ea04
Aleksei Akhrymenka <aleksei.akhrymenka@altoros.com>
2015-04-22 17:08:40 +0300

    update couchbase cluster initialization script: add --bucket-replica option

--- util/trycsb/setup_scripts/couchbase/ubuntu/bin/cluster_init.sh
+++ util/trycsb/setup_scripts/couchbase/ubuntu/bin/cluster_init.sh
@@ -15,7 +15,7 @@ COUCHBASE_CLUSTER_RAMSIZE="81920"
 COUCHBASE_BUCKET_NAME="default"
 COUCHBASE_BUCKET_TYPE="couchbase"
 COUCHBASE_BUCKET_RAMSIZE="81920"
-COUCHBASE_ENABLE_INDEX_REPLICA="1"
+COUCHBASE_BUCKET_REPLICA="1"
 
 if [ "$1" == "192.155.206.162" ]; then
 
@@ -23,7 +23,7 @@ if [ "$1" == "192.155.206.162" ]; then
     couchbase-cli cluster-init -c ${COUCHBASE_MAIN_NODE}:${COUCHBASE_PORT} --cluster-username=${COUCHBASE_USER} --cluster-password=${COUCHBASE_PASSWORD} --cluster-ramsize=${COUCHBASE_CLUSTER_RAMSIZE} -u ${COUCHBASE_USER} -p ${COUCHBASE_PASSWORD}
 
     echo "create bucket at $COUCHBASE_MAIN_NODE"
-    couchbase-cli bucket-create -c ${COUCHBASE_MAIN_NODE}:${COUCHBASE_PORT} --bucket=${COUCHBASE_BUCKET_NAME} --bucket-type=${COUCHBASE_BUCKET_TYPE} --bucket-ramsize=${COUCHBASE_BUCKET_RAMSIZE} --enable-index-replica=${COUCHBASE_ENABLE_INDEX_REPLICA} --enable-flush=0 --wait -u ${COUCHBASE_USER} -p ${COUCHBASE_PASSWORD}
+    couchbase-cli bucket-create -c ${COUCHBASE_MAIN_NODE}:${COUCHBASE_PORT} --bucket=${COUCHBASE_BUCKET_NAME} --bucket-type=${COUCHBASE_BUCKET_TYPE} --bucket-ramsize=${COUCHBASE_BUCKET_RAMSIZE} --bucket-replica=${COUCHBASE_BUCKET_REPLICA} --enable-flush=0 --wait -u ${COUCHBASE_USER} -p ${COUCHBASE_PASSWORD}
 
     else
     exit 0
